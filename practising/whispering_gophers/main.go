7fced82730e71fdd900def79a0ce8a04d8a04696
Jorge Izquierdo <jorge@izqui.me>
2014-06-30 16:42:24 -0400

    go channels bug?

--- practising/whispering_gophers/main.go
+++ practising/whispering_gophers/main.go
@@ -11,10 +11,12 @@ import (
 
 var (
 	address = flag.String("address", "localhost:3003", "address of peer")
+	port    = flag.String("port", "8080", "your port")
 )
 
 type Message struct {
 	Body string `json:"body"`
+	//Address string `json:"address"`
 }
 
 func init() {
@@ -24,22 +26,63 @@ func init() {
 
 func main() {
 
+	incomingConnection := setupIncomingConnection(*port)
+
+	inputCb := make(chan io.Reader)
+	connectionCb := make(chan net.Conn)
+
+	go runReadInput(inputCb)
+	go runConnectionInput(incomingConnection, connectionCb)
+
 	os.Stdout.Write([]byte("Message: "))
 
-	input := readInput(os.Stdin)
-	os.Stdout.Write([]byte(input))
+	for {
 
-	mes := &Message{Body: string(input)}
+		select {
 
-	connection := setupConnection(*address)
-	writeJSON(mes, connection)
-	fmt.Printf("Sent to %s\n", connection.RemoteAddr())
+		case connection := <-connectionCb:
 
-	_, err := os.Stdout.Write(readInput(connection))
-	panicOnError(err)
+			fmt.Println("never being called")
+			fmt.Println("data " + string(readInput(connection)))
+			//message := parseJSON()
+			//connection.Close()
 
-	connection.Close()
+			//writeOutput([]byte(message.Body), os.Stdout)
 
+		case input := <-inputCb:
+
+			mes := &Message{Body: string(readInput(input))}
+			connection := setupOutgoingConnection(*address)
+			fmt.Println("[debug] sending out data")
+
+			writeOutput(generateJSON(mes), connection)
+
+		}
+	}
+}
+
+func runReadInput(cb chan io.Reader) {
+
+	for {
+
+		in := os.Stdin
+		fmt.Println("[debug] studin input")
+
+		cb <- in
+	}
+}
+
+func runConnectionInput(connection net.Listener, cb chan net.Conn) {
+
+	for {
+
+		fmt.Println("[debug] tcp checker")
+		con, err := connection.Accept()
+		fmt.Println("[debug] tcp2")
+		panicOnError(err)
+		fmt.Println("test")
+		cb <- con
+	}
 }
 
 func readInput(reader io.Reader) []byte {
@@ -51,14 +94,42 @@ func readInput(reader io.Reader) []byte {
 	return buf[0:n]
 }
 
-func writeJSON(mes *Message, writer io.Writer) {
+func writeOutput(content []byte, writer io.Writer) {
 
-	enc := json.NewEncoder(writer)
-	err := enc.Encode(mes)
-	panicOnError(err)
+	writer.Write(content)
 }
 
-func setupConnection(address string) net.Conn {
+func parseJSON(data []byte) (message *Message) {
+
+	err := json.Unmarshal(data, message)
+	panicOnError(err)
+
+	return
+}
+
+func generateJSON(mes *Message) []byte {
+
+	data, err := json.Marshal(mes)
+	panicOnError(err)
+
+	return data
+}
+
+func setupIncomingConnection(port string) net.Listener {
+
+	tcpAddress, err := net.ResolveTCPAddr("tcp", string(":"+port))
+	panicOnError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddress)
+	panicOnError(err)
+
+	writeOutput([]byte("Accepting incoming connections on port "+string(port)+" \n"), os.Stdout)
+
+	return listener
+
+}
+
+func setupOutgoingConnection(address string) net.Conn {
 
 	tcpAddress, err := net.ResolveTCPAddr("tcp", address)
 	panicOnError(err)
