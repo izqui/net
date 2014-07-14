8ab84e18d21b3a3cd88b0ac0ca7464ba11b4262e
Jorge Izquierdo <jorge@izqui.me>
2014-07-14 13:48:24 -0400

    New file test

--- mesh/main.go
+++ mesh/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -65,10 +64,10 @@ func main() {
 			go inputHandler(input)
 
 		case input := <-connectionCb:
-			go incomingConnectionHandler(input)
+			go self.HandleIncomingConnection(input)
 
 		case connection := <-searcherCb:
-			go foundConnectionHandler(connection)
+			go self.HandleConnectionFound(connection)
 		}
 	}
 }
@@ -195,177 +194,9 @@ func inputHandler(input []byte) {
 
 	}
 }
-func foundConnectionHandler(connection *net.UDPConn) {
-
-	//fmt.Println("Found a connection opened. Sending my peer info...")
-	sendPeerInfo(connection)
-	//connection.Close()
-}
-
-func sendPeerInfo(connection *net.UDPConn) {
-
-	mes := &Message{Origin: self}
-	mes.AssignRandomID()
-	messageSent(mes.Id)
-	writeOutput(generateJSON(mes), connection)
-}
-
-func incomingConnectionHandler(input []byte) {
-
-	message := parseJSON(input)
-	resp := isResponse(message.Id)
-
-	if message.Body == "" {
-
-		if err := self.AddConnectedPeer(message.Origin); err == nil {
-
-			fmt.Println("Network: ", self)
-			for _, p := range self.ConnectedPeers {
-
-				if (resp && p.Id != message.Origin.Id) || !resp {
-
-					messageSent(message.Id)
-					respMessage := &Message{Id: message.Id, Origin: self}
-
-					writeOutput(generateJSON(respMessage), setupOutgoingConnection(p.Address))
-				}
-			}
-		}
-
-	} else if message.FinalDestinationId == self.Id {
-		//Message is for me :)
-		fmt.Println("! Message from ", message.Origin.Id, " -> ", message.Body)
-	} else {
-		//Message is not for me. Redirecting it.
-		fmt.Println("Broadcasting message from", message.Origin.Id, "to", message.FinalDestinationId)
-		var next_peer = self.FindNearestPeerToId(message.FinalDestinationId)
-
-		if next_peer != nil {
-
-			message.Destination = next_peer.Address
-			connection := setupOutgoingConnection(message.Destination)
-			writeOutput(generateJSON(message), connection)
-
-			fmt.Println("Sending message to", message.FinalDestinationId, "through", next_peer)
-
-		} else {
-
-			fmt.Println("Couldn't find peer with that id")
-		}
-	}
-}
-
-func readInput(reader io.Reader) []byte {
-	buf := make([]byte, 512)
-	n, err := reader.Read(buf)
-	panicOnError(err)
-	return buf[0:n]
-}
-func writeOutput(content []byte, writer io.Writer) {
-	_, err := writer.Write(content)
-	panicOnError(err)
-}
-func parseJSON(data []byte) *Message {
-
-	message := new(Message)
-	err := json.Unmarshal(data, message)
-	panicOnError(err)
-	return message
-}
-func generateJSON(mes *Message) []byte {
-	data, err := json.Marshal(mes)
-	panicOnError(err)
-	return data
-}
-
-func setupIncomingConnection(address string) *net.UDPConn {
-
-	addr, err := net.ResolveUDPAddr("udp", address)
-	panicOnError(err)
-
-	con, err := net.ListenUDP("udp", addr)
-	panicOnError(err)
-
-	return con
-}
-
-func setupOutgoingConnection(address string) *net.UDPConn {
-	udpAddress, err := net.ResolveUDPAddr("udp", address)
-	panicOnError(err)
-	udpConnection, err := net.DialUDP("udp", nil, udpAddress)
-	panicOnError(err)
-
-	return udpConnection
-}
 
 func panicOnError(err error) {
 	if err != nil && err != io.EOF {
 		panic(err)
 	}
-}
-
-func myIp() string {
-
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		panicOnError(err)
-	}
-	for _, iface := range ifaces {
-		if iface.Flags&net.FlagUp == 0 {
-			continue // interface down
-		}
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue // loopback interface
-		}
-		addrs, err := iface.Addrs()
-		if err != nil {
-			panicOnError(err)
-		}
-
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip == nil || ip.IsLoopback() {
-				continue
-			}
-			ip = ip.To4()
-			if ip == nil {
-				continue // not an ipv4 address
-			}
-			return ip.String()
-		}
-	}
-	panicOnError(errors.New("are you connected to the network?"))
-	return ""
-}
-
-func messageSent(id string) {
-
-	//Check for already added messages
-	for _, m := range sentMessages {
-
-		if m == id {
-			return
-		}
-	}
-
-	sentMessages = append(sentMessages, id)
-}
-
-func isResponse(id string) bool {
-
-	for _, m := range sentMessages {
-
-		if id == m {
-
-			return true
-		}
-	}
-
-	return false
 }
